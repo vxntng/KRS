42872fc66a5ff3da5b687824976758b5519ebb59
vxt2001 <vxntng@gmail.com>
2024-02-08 03:27:41 +0700

    fix

---
+++ KRS/DataAccess/IRepository/IAccountRepository.cs
@@ -0,0 +1,15 @@
+﻿
+using BusinessObjects.Models;
+
+namespace DataAccess.IRepository
+{
+    public interface IAccountRepository
+    {
+        Account GetAccByID(int accID);
+        Account Login(string username, string password);
+		Task InsertAccount(Account car);
+		Task UpdateAccount(Account car);
+        
+        Account GetAccByUsername(string username);
+    }
+}
